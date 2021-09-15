92d9c4ae06096295fea69ce3d56f077bbb3b871e
Akhil <akhil@appeonix.com>
2021-09-15 17:14:30 +0530

    2 menus added

--- lib/list_wheel.dart
+++ lib/list_wheel.dart
@@ -12,59 +12,60 @@ class _ListWheelState extends State<ListWheel> {
     return Scaffold(
       body:  ListWheelScrollView(
           itemExtent: 100.0,
-         diameterRatio: 1.5,
-          offAxisFraction: -0.5,
+         diameterRatio: 3.5,
+          offAxisFraction: 0.0,
 
           children:[
             Container(
               height: 150,
               width: 350,
               decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.amber)),
+              child: Center(child: Text('Test'),),
             ),
             Container(
               height: 150,
               width: 350,
-              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.greenAccent)),
+              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.greenAccent)),  child: Center(child: Text('Test'),),
             ),
             Container(
               height: 150,
               width: 350,
-              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.lightBlue)),
+              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.lightBlue)),  child: Center(child: Text('Test'),),
             ),
             Container(
               height: 150,
               width: 350,
-              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.black)),
+              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.black)),  child: Center(child: Text('Test'),),
             ),
             Container(
               height: 150,
               width: 350,
-              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.white)),
+              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.white)),  child: Center(child: Text('Test'),),
             ),
             Container(
               height: 150,
               width: 350,
-              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.grey)),
+              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.grey)),  child: Center(child: Text('Test'),),
             ),
             Container(
               height: 150,
               width: 350,
-              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.deepOrangeAccent)),
+              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.deepOrangeAccent)),  child: Center(child: Text('Test'),),
             ),
             Container(
               height: 150,
               width: 350,
-              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.deepPurple)),
+              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.deepPurple)),  child: Center(child: Text('Test'),),
             ),
             Container(
               height: 150,
               width: 350,
-              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.yellowAccent)),
+              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.yellowAccent)),  child: Center(child: Text('Test'),),
             ),
             Container(
               height: 150,
               width: 350,
-              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.green)),
+              decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.green)),  child: Center(child: Text('Test'),),
             ),
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HzHomePage(),
     );
   }
@@ -33,7 +34,7 @@ class HzHomePage extends StatefulWidget {
 class _HzHomePageState extends State<HzHomePage> {
   var _color;
   dynamic _opacity;
-  bool _toggled = true;
+
   @override
   void initState() {
     _color = Color(0xFFADD8E6);
@@ -44,196 +45,249 @@ class _HzHomePageState extends State<HzHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Orion'),centerTitle: true,
+      leading: Builder(
+        builder: (context){
+          return IconButton(
+            icon:Image.asset('assets/icons/menu.png',scale: 22.0,color: Colors.white,),
+            onPressed: (){
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        },
+      ),
+        actions: [
+          Builder(
+            builder: (context){
+              return IconButton(
+                icon: Image.asset('assets/icons/right-menu-bars.png',scale: 25.0,color: Colors.white,),
+                onPressed: (){
+                  Scaffold.of(context).openEndDrawer();
+                },
+              );
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child:Column(
+          children: [
+            DrawerHeader(
+              child:Text('Left Drawer'),
+            ),
+            ListTile(leading: Icon(Icons.person),title: Text('Electrical'),trailing: Icon(Icons.arrow_right),),
+            ListTile(leading: Icon(Icons.person),title: Text('Fan'),trailing: Icon(Icons.arrow_right),),
+            ListTile(leading: Icon(Icons.person),title: Text('Lighting'),trailing: Icon(Icons.arrow_right),),
+            ListTile(leading: Icon(Icons.person),title: Text('Plumbing'),trailing: Icon(Icons.arrow_right),),
+            ListTile(leading: Icon(Icons.person),title: Text('Bathroom Fittings'),trailing: Icon(Icons.arrow_right),),
+            ListTile(leading: Icon(Icons.person),title: Text('Appliances'),trailing: Icon(Icons.arrow_right),),
+          ],
+        )
+      ),
+      endDrawer: Drawer(
+          child:Column(
+        children: [
+          ListTile(leading: Icon(Icons.person),title: Text('Top Brands'),),
+          ListTile(leading: Icon(Icons.person),title: Text('Flash Sales'),),
+          ListTile(leading: Icon(Icons.person),title: Text('New Arrivals'),),
+          ListTile(leading: Icon(Icons.person),title: Text('Hot Sales'),),
+          ListTile(leading: Icon(Icons.person),title: Text('Offer Zone'),),
+          ListTile(leading: Icon(Icons.person),title: Text('Combo Offers'),),
+        ],
+      )
+      ),
       body: SafeArea(
         child: Container(
           height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 160,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 1, color: Colors.black12),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Hero(
-                          tag: 'mario',
-                          child: GestureDetector(
-                            child: Image.network(
-                              'https://icons.iconarchive.com/icons/ph03nyx/super-mario/256/Paper-Mario-icon.png',
-                              width: 100.0,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 160,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.black12),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Hero(
+                            tag: 'mario',
+                            child: GestureDetector(
+                              child: Image.network(
+                                'https://icons.iconarchive.com/icons/ph03nyx/super-mario/256/Paper-Mario-icon.png',
+                                width: 100.0,
+                              ),
+                              onTap: () => Navigator.push(
+                                  context,
+                                  PageRouteBuilder(
+                                      transitionDuration: Duration(seconds: 2),
+                                      pageBuilder: (_, __, ___) => Details())),
                             ),
-                            onTap: () => Navigator.push(
-                                context,
-                                PageRouteBuilder(
-                                    transitionDuration: Duration(seconds: 2),
-                                    pageBuilder: (_, __, ___) => Details())),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            'SUPER MARIO',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Container(
-                    height: 160,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 1, color: Colors.black12),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Hero(
-                          tag: 'spider',
-                          child: GestureDetector(
-                            child: Image.network(
-                              'https://cdna.artstation.com/p/assets/images/images/019/221/596/original/hibban-mohammed-ezgif-com-gif-maker.gif?1562575662',
-                              width: 100.0,
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'SUPER MARIO',
+                              textAlign: TextAlign.center,
                             ),
-                            onTap: () => Navigator.push(
-                                context,
-                                PageRouteBuilder(
-                                    transitionDuration: Duration(seconds: 2),
-                                    pageBuilder: (_, __, ___) =>
-                                        SpiderDetails())),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            'SPIDERMAN',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  AnimatedContainer(
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      height: 160,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.black12),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Hero(
+                            tag: 'spider',
+                            child: GestureDetector(
+                              child: Image.network(
+                                'https://cdna.artstation.com/p/assets/images/images/019/221/596/original/hibban-mohammed-ezgif-com-gif-maker.gif?1562575662',
+                                width: 100.0,
+                              ),
+                              onTap: () => Navigator.push(
+                                  context,
+                                  PageRouteBuilder(
+                                      transitionDuration: Duration(seconds: 2),
+                                      pageBuilder: (_, __, ___) =>
+                                          SpiderDetails())),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'SPIDER MAN',
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 1000),
+                      height: 160,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.black12),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Hero(
+                            tag: 'hulk',
+                            child: GestureDetector(
+                              child: Image.network(
+                                'https://www.nicepng.com/png/full/10-104779_incredible-hulk-png-hulk-comic-png.png',
+                                width: 100.0,
+                              ),
+                              onTap: () => Navigator.push(
+                                  context,
+                                  PageRouteBuilder(
+                                      transitionDuration: Duration(seconds: 2),
+                                      pageBuilder: (_, __, ___) =>
+                                          HulkDetails())),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Hulk',
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      height: 160,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.black12),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Hero(
+                            tag: 'batman',
+                            child: GestureDetector(
+                              child: Image.network(
+                                'https://purepng.com/public/uploads/large/purepng.com-arkham-batmanbatmansuperherocomicdc-comicsbob-kanebat-manbruce-wayne-1701528523764v4kbc.png',
+                                width: 100.0,
+                              ),
+                              onTap: () => Navigator.push(
+                                  context,
+                                  PageRouteBuilder(
+                                      transitionDuration: Duration(seconds: 2),
+                                      pageBuilder: (_, __, ___) =>
+                                          BatmanDetails())),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'BATMAN',
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Opacity(
+                  opacity: 0.25,
+                  child: AnimatedContainer(
                     duration: Duration(milliseconds: 1000),
-                    height: 160,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 1, color: Colors.black12),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Hero(
-                          tag: 'hulk',
-                          child: GestureDetector(
-                            child: Image.network(
-                              'https://www.nicepng.com/png/full/10-104779_incredible-hulk-png-hulk-comic-png.png',
-                              width: 100.0,
-                            ),
-                            onTap: () => Navigator.push(
-                                context,
-                                PageRouteBuilder(
-                                    transitionDuration: Duration(seconds: 2),
-                                    pageBuilder: (_, __, ___) =>
-                                        HulkDetails())),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            'Hulk',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
-                    ),
+                    height: 200,
+                    color: _color,
+                    width: 200,
+                    curve: Curves.bounceIn,
                   ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Container(
-                    height: 160,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 1, color: Colors.black12),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Hero(
-                          tag: 'batman',
-                          child: GestureDetector(
-                            child: Image.network(
-                              'https://purepng.com/public/uploads/large/purepng.com-arkham-batmanbatmansuperherocomicdc-comicsbob-kanebat-manbruce-wayne-1701528523764v4kbc.png',
-                              width: 100.0,
-                            ),
-                            onTap: () => Navigator.push(
-                                context,
-                                PageRouteBuilder(
-                                    transitionDuration: Duration(seconds: 2),
-                                    pageBuilder: (_, __, ___) =>
-                                        BatmanDetails())),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            'BATMAN',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Opacity(
-                opacity: 0.25,
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 1000),
-                  height: 200,
-                  color: _color,
-                  width: 200,
-                  curve: Curves.bounceIn,
                 ),
-              ),
-              AnimatedOpacity(
-                opacity: _opacity,
-                duration: Duration(milliseconds: 1000),
-                child: AnimatedContainer(
+                AnimatedOpacity(
+                  opacity: _opacity,
                   duration: Duration(milliseconds: 1000),
-                  height: 200,
-                  color: _color,
-                  width: 200,
-                  curve: Curves.bounceIn,
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 1000),
+                    height: 200,
+                    color: _color,
+                    width: 200,
+                    curve: Curves.bounceIn,
+                  ),
                 ),
-              ),
-              ElevatedButton(
-                  onPressed: () => Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ListWheel())),
-                  child: Text('click')),
-            ],
+                ElevatedButton(
+                    onPressed: () => Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => ListWheel())),
+                    child: Text('click')),
+              ],
+            ),
           ),
         ),
       ),
